b3305b316c7ecf892520d2231ac6cc5d24d3b9cc
BeeMao03 <153680716+BeeMao03@users.noreply.github.com>
2024-02-26 20:57:05 -0300

    Add axios and weather app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import SearchEngine from "./SearchEngine";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Weather App</h1>
+      <SearchEngine />
+      <div>
+        <small>
+          <a>Open-sourced code</a> by{" "}
+          <a href="https://github.com/BeeMao03">Mariel Buzarquis</a> hosted on{" "}
+          <a>Netlify</a>
+        </small>
+      </div>
     </div>
   );
 }
