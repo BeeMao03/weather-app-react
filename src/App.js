ad78e3dcf72bea43351afe370d68d7444f01dae6
BeeMao03 <153680716+BeeMao03@users.noreply.github.com>
2024-02-26 21:18:47 -0300

    Add href for netlify and space for footer

--- src/App.js
+++ src/App.js
@@ -7,12 +7,13 @@ function App() {
       <h1>Weather App</h1>
       <SearchEngine />
       <div>
+        <br />
         <small>
           <a href="https://github.com/BeeMao03/weather-app-react">
             Open-sourced code
           </a>{" "}
           by <a href="https://github.com/BeeMao03">Mariel Buzarquis</a> hosted
-          on Netlify
+          on <a href="https://loquacious-sorbet-47ac4a.netlify.app/">Netlify</a>
         </small>
       </div>
     </div>
